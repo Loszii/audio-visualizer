10460d5a832e1e459d53a241cc7d9f0c79c0d69d
Loszi <jiozzi11@gmail.com>
2023-12-23 19:47:44 -0500

    Changed drawLines(), now has a simplification factor to display more or less lines, a dark trail and brighter tip. The trail and speed of each line is the average freq of song * frequency of line * scale. Uses averages and slicing to simplify frequencies through new sampleSimplifier

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ canvas.height = window.innerHeight;
 let tempVar = 0;
 let bufferLength;
 let xTracker;
+let newData;
+let simplificationFactor = 4;
 
 //colors
 let red = 255;
@@ -47,7 +49,10 @@ file.addEventListener("change", function(){
     //only want 20 out of 24 hz freq since that is human hearing range
     bufferLength = Math.round(analyzer.frequencyBinCount * (20/24)); // always half of fft size and number of bars
     const dataArray = new Uint8Array(bufferLength); // array of unsigned integers up to 2^8, will be of length bufferLength
-    xTracker = new Array(bufferLength);
+
+    //for drawLines()
+    xTracker = new Array(Math.round(bufferLength / simplificationFactor));
+    newData = new Array(Math.round(bufferLength / simplificationFactor));
     initXTracker();
 
     //animation loop
@@ -106,7 +111,7 @@ function drawData(dataArray){
 }
 
 function drawCircle(dataArray){
-    const rotations = 10;
+    const rotations = 10; //not exactly ten since bufferLength equation is irrational number
     const barWidth = 5
     ctx.save(); //saves canvas
     ctx.translate(canvas.width / 2, canvas.height / 2); //setting origin to middle for rotation
@@ -143,32 +148,50 @@ function drawBars(dataArray){
     }
 }
 
+
+
 function initXTracker(){
-    for(let i = 0; i < bufferLength; i++){
+    for(let i = 0; i < xTracker.length; i++){
         xTracker[i] = 0;
     }
 }
 
+function sampleSimplifier(dataArray){
+    let prevIndex = 0;
+    let dataIndex = 0
+    for(let i = 1; i < Math.round(bufferLength / simplificationFactor) + 1; i ++){
+        newData[dataIndex] = getAverage(dataArray.slice(prevIndex, i * simplificationFactor));
+        prevIndex = i * simplificationFactor;
+        dataIndex += 1;
+    }
+    return newData;
+}
+
 function drawLines(dataArray){
-    const scale = 0.05;
+    const scale = 0.035; //scale of speed
     let y = 110;
-    const barHeight = canvas.height / bufferLength;
-    for(let i = 0; i < bufferLength; i++){
-        let barWidth = getAverage(dataArray);
-        ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")";
-        if (xTracker[i] > canvas.width){
+    newData = sampleSimplifier(dataArray);
+    newDataLength = newData.length;
+    const barHeight = canvas.height / newDataLength;
+    for(let i = 0; i < newDataLength; i++){ //to put back on screen
+        if (xTracker[i] - 600 > canvas.width){
             xTracker[i] = 0;
         }
-        ctx.fillRect(xTracker[i], y, barWidth, barHeight);
-        xTracker[i] += (scale * dataArray[i]);
+        let barWidth = scale * getAverage(newData) * newData[i]; //width of trail and value used for speed
+        ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")";
+        ctx.fillRect(xTracker[i], y, barHeight, barHeight); //square in front
+        ctx.fillStyle = "rgb(" + red*0.5 + "," + green*0.5 + "," + blue*0.5 + ")";
+        ctx.fillRect(xTracker[i], y, -1 * barWidth, barHeight); //trail
+        xTracker[i] += barWidth; //movement
         y += barHeight;
     }
 }
 
-function getAverage(dataArray){
+function getAverage(array){
     let sum = 0;
-    for(let i = 0; i < bufferLength; i++){
-        sum += dataArray[i];
+    let length = array.length
+    for(let i = 0; i < length; i++){
+        sum += array[i];
     }
-    return sum / bufferLength;
+    return sum / length;
 }
