4abbb68c979f91347ad059962ac048b3899c3bad
Loszi <jiozzi11@gmail.com>
2023-12-27 23:15:50 -0500

    added drawSquares() and drawSquare() for new visualizer mode. also created button and changed some div widths to fit extra button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//to do: more visualizers, make module
+//to do: make module and custon audio controls
 
 const canvas = document.getElementById("canvas1");
 const container = document.getElementById("container");
@@ -15,6 +15,7 @@ const audio1 = document.getElementById("audio1");
 const barButton = document.getElementById("barButton");
 const circleButton = document.getElementById("circleButton");
 const lineButton = document.getElementById("lineButton");
+const squareButton = document.getElementById("squareButton");
 
 //sliders
 const redSlider = document.getElementById("redSlider");
@@ -23,21 +24,21 @@ const blueSlider = document.getElementById("blueSlider");
 
 //coloring and memorization
 let visMode = 0;
-if ("visMode" in sessionStorage){
+if ("visMode" in sessionStorage) {
     visMode = sessionStorage.getItem("visMode");
 }
 let red = parseInt(redSlider.value);
-if ("red" in sessionStorage){
+if ("red" in sessionStorage) {
     red = sessionStorage.getItem("red");
     redSlider.value = sessionStorage.getItem("red");
 }
 let green = parseInt(greenSlider.value);
-if ("green" in sessionStorage){
+if ("green" in sessionStorage) {
     green = sessionStorage.getItem("green");
     greenSlider.value = sessionStorage.getItem("green");
 }
 let blue = parseInt(blueSlider.value);
-if ("blue" in sessionStorage){
+if ("blue" in sessionStorage) {
     blue = sessionStorage.getItem("blue");
     blueSlider.value = sessionStorage.getItem("blue");
 }
@@ -113,6 +114,10 @@ lineButton.addEventListener("click", function(){
     visMode = 2;
     sessionStorage.setItem("visMode", 2);
 });
+squareButton.addEventListener("click", function(){
+    visMode = 3;
+    sessionStorage.setItem("visMode", 3)
+});
 
 //sliders and coloring
 redSlider.addEventListener("input", function(){
@@ -132,6 +137,7 @@ function changeColor(){
     barButton.style = "border-color: rgb(" + red + ", " + green + ", " + blue + ");";
     circleButton.style = "border-color: rgb(" + red + ", " + green + ", " + blue + ");";
     lineButton.style = "border-color: rgb(" + red + ", " + green + ", " + blue + ");";
+    squareButton.style = "border-color: rgb(" + red + ", " + green + ", " + blue + ");";
     but.style = "border-color: rgb(" + red + ", " + green + ", " + blue + ");";
     head.style = "text-decoration: underline rgb(" + red + ", " + green + ", " + blue + "); text-shadow: 3px 2px 4px rgb(" + red + ", " + green + ", " + blue + ");";
     audio1.style = "filter: drop-shadow(0px 0px 10px rgb(" + red + ", " + green + ", " + blue + "))";
@@ -142,12 +148,14 @@ function changeColor(){
 
 //draw algorithms
 function drawData(dataArray){
-    if (visMode == 0){
+    if (visMode == 0) {
         drawBars(dataArray);
-    } else if(visMode == 1){
+    } else if (visMode == 1) {
         drawCircle(dataArray);
-    } else{
+    } else if (visMode == 2) {
         drawLines(dataArray);
+    } else {
+        drawSquares(dataArray);
     }
 }
 
@@ -156,7 +164,7 @@ function drawCircle(dataArray){
     const barWidth = 5;
     ctx.save(); //saves canvas
     ctx.translate(canvas.width / 2, canvas.height / 2); //setting origin to middle for rotation
-    for(let i = 0; i < bufferLength; i++){
+    for (let i = 0; i < bufferLength; i++) {
         let heightScale = (1.75 - (0.003 * i));
         let darkScale = 0.5 * i; //make color slowly darker
         ctx.fillStyle = "black";
@@ -171,9 +179,9 @@ function drawCircle(dataArray){
 function drawBars(dataArray){
     const barWidth = ((canvas.width/2) / bufferLength); //divided by 2 for mirrored image
     let x = 0;
-    for(let i = 0; i < bufferLength; i++){
+    for (let i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i] * 2;
-        if (barHeight > 0){
+        if (barHeight > 0) {
             ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")";
             ctx.fillRect((canvas.width / 2) - x, canvas.height - barHeight - 15, barWidth, 15);
             ctx.fillStyle = "rgb(" + red*0.5 + "," + green*0.5 + "," + blue*0.5 + ")";
@@ -181,9 +189,9 @@ function drawBars(dataArray){
         }
         x += barWidth;
     }
-    for(let i = 0; i < bufferLength; i++){
+    for (let i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i] * 2;
-        if (barHeight > 0){
+        if (barHeight > 0) {
             ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")";
             ctx.fillRect(x, canvas.height - barHeight - 15, barWidth, 15);
             ctx.fillStyle = "rgb(" + red*0.5 + "," + green*0.5 + "," + blue*0.5 + ")";
@@ -194,7 +202,7 @@ function drawBars(dataArray){
 }
 
 function initXTracker(){
-    for(let i = 0; i < bufferLength; i++){
+    for (let i = 0; i < bufferLength; i++) {
         xTracker[i] = 0;
     }
 }
@@ -204,15 +212,13 @@ function drawLines(dataArray){
     const barHeight = canvas.height / bufferLength;
     let y = 115;
     let avg = getAverage(dataArray);
-    for(let i = 0; i < bufferLength; i++){ //to put back on screen
+    ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")";
+    for (let i = 0; i < bufferLength; i++) { //to put back on screen
         let velocity = scale * avg * dataArray[i]; //width of trail and value used for speed increment
-        if (xTracker[i] - velocity > canvas.width){ //once edge of trail is off screen reset
+        if (xTracker[i] - velocity > canvas.width) { //once edge of trail is off screen reset
             xTracker[i] = -0.25 * velocity; //reset based on trail length
         }
-        if (velocity > 0){ //remove non moving lines
-            ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")";
-            ctx.fillRect(xTracker[i], y, barHeight, barHeight); //square in front
-            ctx.fillStyle = "rgb(" + red*0.5 + "," + green*0.5 + "," + blue*0.5 + ")";
+        if (velocity > 0) { //remove non moving lines
             ctx.fillRect(xTracker[i], y, -0.75 * velocity, barHeight); //trail 3/4 of velocity
             xTracker[i] += velocity; //movement
         }
@@ -220,17 +226,40 @@ function drawLines(dataArray){
     }
 }
 
+function drawSquares(dataArray){
+    const scale = 0.015;
+    const y = 30;
+    let avg = getAverage(dataArray);
+    ctx.lineWidth = 0.035;
+    ctx.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
+    for (let i = 0.5; i < 10; i++) {
+        drawSquare(dataArray, i/10, 4/10,scale, y, avg);
+        drawSquare(dataArray, i/10, 6/10,scale, y, avg);
+    }
+    ctx.stroke();
+}
+
+function drawSquare(dataArray, xPos, yPos, scale, y, avg){
+    ctx.save();
+    ctx.translate(canvas.width * xPos, canvas.height *yPos + y);
+    for (let i = 0; i < bufferLength; i++) {
+        let squareLength = avg * scale * dataArray[i];
+        ctx.strokeRect(-1 * squareLength / 2, -1 * squareLength / 2, squareLength, squareLength);
+    }
+    ctx.restore();
+}
+
 function getAverage(array){
     let sum = 0;
     let length = array.length;
-    for(let i = 0; i < length; i++){
+    for (let i = 0; i < length; i++) {
         sum += array[i];
     }
     return sum / length;
 }
 
 function getSongName(name){
-    if (name.length > 20){
+    if (name.length > 20) {
         name = name.slice(0, 21) + "...";
     }
     return name;
